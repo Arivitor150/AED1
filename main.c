6e29546edd6cc157b3e32b5f5bede44fcd17dce1
Azshalasa <mts.c@live.com>
2018-09-06 19:13:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,72 +2,35 @@
 #include <stdlib.h>
 #include "lib.h"
 
-void attVar(void *pBuffer, void **op, void **count, void **lp){
-    *op = pBuffer;
-    *lp = pBuffer+sizeof(int);
-    *count = pBuffer + (2*sizeof(int));
+int main()
+{
+    void *pBuffer;
+    void *op;
+    void *count;
+    void *lp;
 
-}
+    pBuffer = (void*)malloc((sizeof(int)*3));
+    *(int *)pBuffer = 0; // Menu de Opção
+    *(int *)(pBuffer + sizeof(int)) = 0; // Looping
+    *(int *)(pBuffer + (2*sizeof(int))) = 0; // Variavel Contadora
 
-void adcTo(void *pBuffer, int *count){
+    attVar(pBuffer, &op, &count, &lp);
+    adcTo(pBuffer, count);
+    attVar(pBuffer, &op, &count, &lp);
 
-        *count = *count + 1;
-
-        pBuffer = realloc(pBuffer, (3 * sizeof(int)) + ( (*count) * sizeof(tipoagenda)));
-        tipoagenda * test;
-
-        test = pBuffer + (3 * sizeof(int)) + ( (*(int *)(pBuffer + (2*sizeof(int)))) * sizeof(tipoagenda));
-
-        printf("Digite o Numero\n");
-        scanf("%d",&test->cfone);
-        printf("Digite o Nome\n");
-        //__fpurge(stdin);
-        fgets (test->nome , (sizeof(char)*10) , stdin);
-
-}
-
-void printAll(void *pBuffer, void *count, void *lp){
-    printf("Buffer: %p\n", pBuffer + (3 * sizeof(int)) + ( (*(int *)count) * sizeof(tipoagenda)));
-
-    if(*(int *)count > 0){
-        tipoagenda * test;
-        for(*(int *)lp = 0; (*(int *)lp) < (*(int *)count); (*(int *)lp)++){
-
-                test = pBuffer + (3 * sizeof(int)) + ( (*(int *)lp) * sizeof(tipoagenda));
-                printf("\n");
-                printf("Phone:%d\n", test->cfone);
-                printf("Name:%s\n", test->nome);
-                printf("\n");
-        }
-}
-}
-
-void menu(){
-        /*pBuffer = realloc(pBuffer, (sizeof(pBuffer) + sizeof(tipoagenda)));
-        tipoagenda * test;
-        test = pBuffer + (sizeof(int)*3) + sizeof(tipoagenda);
-        test->cphone = 999;
-        test->name[0] = 't';
-        printf("R2:%d\n", sizeof(pBuffer));
-        printf("Ce : %d\n", &test->cphone);
-        printf("No : %d\n", &test->name[0]);
-        // *(int *)pBuffer = 7;
-        // *(int *)(pBuffer + sizeof(int)) = 9;
-        // *op = 10;
-        // *lp = 5;
-        //printf("RefV : %d\n", *(int *)pBuffer);
-        */
+    tipoagenda * test;
+    test = pBuffer + (sizeof(int)*3) + ( *(int *)count * sizeof(tipoagenda));
+    printf("Phone : %d\n", test->cfone);
+    printf("Nome  : %s \n", test->nome);
 
 
- //   tipoagenda * test;
-  //  test = pBuffer + (sizeof(int)*3) + sizeof(tipoagenda);
-   // printf("Ce : %d\n", test->cphone);
-   // printf("No : %d\n", test->name[0]);
-    //printf("M: %d\n", pBuffer);
+    /*AddTo(pBuffer, *(int *)op);
+    refreshVar(pBuffer, &op, &count, &lp);
+    test = pBuffer + (sizeof(int)*3) + ( *(int *)count * sizeof(tipoagenda));
+    printf("Phone : %d\n", test->fone);
+    printf("Nome  : %s \n", test->nome); */
 
-        //printf("Main : \n");
-   // printf("OP : %i\n", op);
-   // printf("LP : %i\n", *(int *)lp);
-    //printf("CO : %i\n", *(int *)counT);
 
+
+    return 0;
 }
