58ea58883c967d9431060e46a34423354e2340d8
Ari Vitor Lazzarotto <38007325+Arivitor150@users.noreply.github.com>
2018-09-29 16:03:35 -0300

    Delete lib.h

--- lib.h
+++
@@ -1,9 +0,0 @@
-typedef struct Agenda{
-    int cfone;
-    char nome[10];
-}tipoagenda;
-
-void menu();
-void attVar(void *pBuffer, void **op, void **count, void **lp);
-void adcTo(void *pBuffer, int *count);
-void printAll(void *pBuffer, void *count, void *lp);
